a41f9c705fcd1cd18f9dd94ff92088f255ffba83
whmcquaid <whmcquaid@gmail.com>
2021-02-18 22:14:21 -0500

    Updates

---
+++ precache-manifest.c1120b8254d09d2a97d634bdcd50fd1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0a9de03baf1117f1d3646a164555ce",
+    "url": "/React-RMDB/index.html"
+  },
+  {
+    "revision": "fc55626c2390fbe37fff",
+    "url": "/React-RMDB/static/js/2.15a7495e.chunk.js"
+  },
+  {
+    "revision": "e345ebe23f280db39f71",
+    "url": "/React-RMDB/static/js/main.d9af5b8a.chunk.js"
+  },
+  {
+    "revision": "70421245cee208e28669",
+    "url": "/React-RMDB/static/js/runtime~main.e01aae73.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/React-RMDB/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/React-RMDB/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/React-RMDB/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
